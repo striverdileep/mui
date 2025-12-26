81397323b82f9c8909306cd94d01a9099e2da201
Dileep <striverdileep@gmail.com>
2025-12-26 18:51:41 +0530

    Completed MUI BottomNavigation

---
+++ src/components/MuiBottomNavigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+const MuiBottomNavigation = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <BottomNavigation
+      sx={{ position: "absolute", bottom: 0, margin: "auto" }}
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+      <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+    </BottomNavigation>
+  );
+};
+
+export default MuiBottomNavigation;
